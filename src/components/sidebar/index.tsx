e2e9c55176f6a59b32d5a3672dd0de86770eb9e8
mayuri <mayurigurav1997@gmail.com>
2024-02-10 23:04:03 +0530

    Layout created

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import HomeIcon from '../icons/home-icon'
+import MovieIcon from '../icons/movie-icon'
+import TvSeriesIcon from '../icons/series-icon'
+import BookmarkIcon from '../icons/bookmark-icon'
+import { useLocation } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+const Sidebar = () => {
+    const navLinks = [
+        {
+            name:"Home",
+            icon:HomeIcon,
+            link:"/"
+        },{
+            name:"Movies",
+            icon:MovieIcon,
+            link:"/movie"
+        },{
+            name:"Tv Series",
+            icon:TvSeriesIcon,
+            link:"/tv-series"
+        },{
+            name:"Bookmarks",
+            icon:BookmarkIcon,
+            link:"/bookmark"
+        },
+
+    ]
+    const {pathname}= useLocation()
+  return (
+    <Box sx={{backgroundColor:"#161d2f",
+    padding:2,
+    borderRadius:2,
+    display:"flex",
+    flexDirection:{
+        xs:"row",
+        lg:"column",},
+        alignItems:"center",
+        justifyContent:"space-between",
+        width:{
+            sm:"100%",
+            lg:200
+        }
+        }}>
+      
+    </Box>
+  )
+}
+
+export default Sidebar
