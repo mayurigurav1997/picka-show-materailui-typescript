b4227db7ad042d99c955f0c7a8e9a46d02f41b60
mayuri <mayurigurav1997@gmail.com>
2024-02-10 23:37:43 +0530

    Sidebar created

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,51 +1,137 @@
-import React from 'react'
-import HomeIcon from '../icons/home-icon'
-import MovieIcon from '../icons/movie-icon'
-import TvSeriesIcon from '../icons/series-icon'
-import BookmarkIcon from '../icons/bookmark-icon'
-import { useLocation } from 'react-router-dom'
-import { Box } from '@mui/material'
+import React from "react";
+import homeIcon from "../../assets/icons/icon-nav-home.svg";
+import movieIcon from "../../assets/icons/icon-nav-movies.svg";
+import tvSeriesIcon from "../../assets/icons/icon-nav-tv-series.svg";
+import bookmarkIcon from "../../assets/icons/icon-nav-bookmark.svg";
+import { Link, useLocation } from "react-router-dom";
+import { Box, Hidden, Typography } from "@mui/material";
 
+const navLinks = [
+    {
+      name: "Home",
+      icon: homeIcon,
+      link: "/home",
+    },
+    {
+      name: "Login",
+      icon: homeIcon,
+      link: "/",
+    },
+    {
+      name: "Movies",
+      icon: movieIcon,
+      link: "/movies",
+    },
+    {
+      name: "TV Series",
+      icon: tvSeriesIcon,
+      link: "/tv-series",
+    },
+    {
+      name: "Bookmarks",
+      icon: bookmarkIcon,
+      link: "/bookmark",
+    },
+  ];
 const Sidebar = () => {
-    const navLinks = [
-        {
-            name:"Home",
-            icon:HomeIcon,
-            link:"/"
-        },{
-            name:"Movies",
-            icon:MovieIcon,
-            link:"/movie"
-        },{
-            name:"Tv Series",
-            icon:TvSeriesIcon,
-            link:"/tv-series"
-        },{
-            name:"Bookmarks",
-            icon:BookmarkIcon,
-            link:"/bookmark"
-        },
-
-    ]
-    const {pathname}= useLocation()
+  
+  const { pathname } = useLocation();
   return (
-    <Box sx={{backgroundColor:"#161d2f",
-    padding:2,
-    borderRadius:2,
-    display:"flex",
-    flexDirection:{
-        xs:"row",
-        lg:"column",},
-        alignItems:"center",
-        justifyContent:"space-between",
-        width:{
-            sm:"100%",
-            lg:200
-        }
-        }}>
-      
+    <Box
+      sx={{
+        backgroundColor: "#161d2f",
+        padding: 2,
+        borderRadius: 2,
+        display: "flex",
+        flexDirection: {
+          xs: "row",
+          lg: "column",
+        },
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: {
+          sm: "100%",
+          lg: 200,
+        },
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "row",
+            lg: "column",
+          },
+          gap: 5,
+          alignItems: {
+            xs: "center",
+            lg: "start",
+          },
+          width: "100%",
+        }}
+      >
+        <Hidden smDown>
+          <Typography
+            variant="h5"
+            component="h1"
+            my={2}
+            fontWeight={400}
+            fontSize={18}
+          >
+            PikaShow App
+          </Typography>
+        </Hidden>
+        <Box
+          sx={{
+            py: {
+              xs: "0px",
+              lg: "16px",
+            },
+            display: "flex",
+            flexDirection: {
+              xs: "row",
+              lg: "column",
+            },
+            gap: 4,
+          }}
+        >
+          {navLinks.map((item) => (
+            <Link
+              key={item.name}
+              to={item.link}
+              style={{ textDecoration: "none" }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 2,
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                <img
+                  src={item.icon}
+                  alt={item.name}
+                  style={{
+                    width: "18px",
+                    filter: `${
+                      pathname === item.link
+                        ? "invert(58%) sepia(14%) saturate(3166%) hue-rotate(215deg) brightness(91%) contrast(87%)"
+                        : "invert(84%)"
+                    }`,
+                  }}
+                />
+                <Hidden mdDown>
+                  <Typography>{item.name}</Typography>
+                </Hidden>
+              </Box>
+            </Link>
+          ))}
+        </Box>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
